afe383239e2c4a0ae2a0384aa28e852e31c22845
begasanna <anneta.begas@gmail.com>
2019-01-31 23:06:44 +0200

    app weather

--- begas-app/src/App.js
+++ begas-app/src/App.js
@@ -2,27 +2,62 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const CITY = [
+  { name: "Palo Alto", zip: "11111" },
+  { name: "San Jose", zip: "22222" },
+  { name: "Santa Cruz", zip: "33333" },
+  { name: "Honolulu", zip: "44444" }
+];
+
+class Weather extends Component{
+  render(){
+    return (
+      <h1>Мой первый сюжет о погоде для города {this.props.zip} </h1>
+    )
+  }
+}
+
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      activePlace: 1,
+    };
+  }
   render() {
+    const activePlace = this.state.activePlace;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Я выучу Реакт за три месяца!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+
+        {CITY.map((place, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              this.setState({activePlace:index})
+            }}
+          >
+              {place.name}
+          </button>
+    ))}
+    <Weather
+        key = {activePlace}
+        zip = {CITY[activePlace].zip} />
       </div>
     );
   }
 }
 
+
+
+
 export default App;
+// export Weather;
